2d3841d734dcefb95ca665e2d6ec70edd2dbc2d2
Zach Fuller <zach.fuller1222@gmail.com>
2024-12-26 21:09:50 -0700

    feat: starred repos are saved to json file

--- src/github_feed/app.py
+++ src/github_feed/app.py
@@ -1,11 +1,25 @@
+import pathlib
 from os import environ
 
 from rich.pretty import pprint
 from rich.traceback import install
 
 from github_feed.github_client import GitHubClient
+from github_feed.models import Repository
 
 install(show_locals=False)
+
+
+def save_starred_repos(repos: list[Repository], filename: str) -> None:
+    with pathlib.Path(filename).open("w") as file:
+        file.write("[\n")
+        for i, repo in enumerate(repos):
+            file.write(f"{repo.model_dump_json(indent=2)}")
+            if i < len(repos) - 1:
+                file.write(",\n")
+            else:
+                file.write("\n")
+        file.write("]\n")
 
 
 def retrieve_activity() -> None:
@@ -14,7 +28,7 @@ def retrieve_activity() -> None:
     resp = client.get_user()
     pprint(resp)
     starred = client.get_starred_repositories()
-    pprint(starred)
+    save_starred_repos(starred, "starred_repos.json")
 
 
 if __name__ == "__main__":
