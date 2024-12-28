cb462033100724d19b576f96e71c3f8958cb5765
Zach Fuller <zach.fuller1222@gmail.com>
2024-12-28 16:39:51 -0700

    poc generate list of collapsible blocks as list items

--- src/github_feed/components/releases_list.py
+++ src/github_feed/components/releases_list.py
@@ -2,7 +2,15 @@ from typing import Any
 
 from textual.app import ComposeResult
 from textual.widget import Widget
-from textual.widgets import Label, ListItem, ListView
+from textual.widgets import Collapsible, Label, ListItem, ListView, Markdown
+
+JESSICA = """
+# Lady Jessica
+
+Bene Gesserit and concubine of Leto, and mother of Paul and Alia.
+"""
+
+names = ["Paul", "Leto", "Alia", "Ghanima", "Irulan", "Chani", "Stilgar", "Duncan", "Thufir", "Gurney"]
 
 
 class ReleasesList(Widget):
@@ -10,4 +18,13 @@ class ReleasesList(Widget):
         super().__init__(**kwargs)
 
     def compose(self) -> ComposeResult:
-        yield ListView(ListItem(Label("1")), ListItem(Label("2")))
+        items: list[ListItem] = [
+            ListItem(Collapsible(Label(f"Nested content for {name}"), title=name)) for name in names
+        ]
+
+        yield ListView(
+            ListItem(Label("1")),
+            ListItem(Label("2")),
+            ListItem(Collapsible(Markdown(JESSICA), collapsed=False, title="Jessica")),
+            *items,
+        )
