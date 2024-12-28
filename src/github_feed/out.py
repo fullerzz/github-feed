4443d255b8142e9bda5b5e4b676ff03a8f3a5f1a
Zach Fuller <zach.fuller1222@gmail.com>
2024-12-27 18:06:23 -0700

    example html and svg output

--- src/github_feed/out.py
+++ src/github_feed/out.py
@@ -1,13 +1,15 @@
 from rich.box import HEAVY
+from rich.columns import Columns
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.panel import Panel
 from rich.table import Table
+from rich.terminal_theme import NIGHT_OWLISH
 
 from github_feed import utils
 from github_feed.models import Release
 
-console = Console()
+console = Console(record=True)
 
 RELEASE_COLUMNS = ["html_url", "tag_name", "created_at"]
 
@@ -29,17 +31,19 @@ def display_releases(releases: list[Release]) -> None:
 
 
 def display_releases_panels(releases: list[Release]) -> None:
+    panels: list[Panel] = []
     for release in releases:
         repo_name = utils.extract_repo_name_from_html_url(release.html_url)
         title = f"[bold cyan]{repo_name}[/bold cyan] - [bold green]{release.tag_name}[/]"
-        console.print(
-            Panel(
-                Markdown(release.body, code_theme="one-dark", justify="left"),
-                box=HEAVY,
-                title=title,
-                expand=False,
-                highlight=True,
-                padding=(0, 2),
-            ),
-            new_line_start=True,
+        panel = Panel(
+            Markdown(release.body, code_theme="one-dark", justify="left"),
+            box=HEAVY,
+            title=title,
+            expand=False,
+            highlight=True,
+            padding=(0, 2),
         )
+        console.print(panel, new_line_start=True)
+        panels.append(panel)
+
+    console.save_svg("releases.svg", title="Starred Repos", theme=NIGHT_OWLISH)
